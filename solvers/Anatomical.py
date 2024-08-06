24a0a66641d53653c02b51631e2440c8d1ff0d0a
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-08-06 17:52:51 +0200

    chore: Rename identity solver to Anatomical

--- solvers/Anatomical.py
+++ solvers/Anatomical.py
@@ -17,7 +17,7 @@ with safe_import_context() as import_ctx:
 # inherit from `BaseSolver` for `benchopt` to work properly.
 class Solver(BaseSolver):
     # Name to select the solver in the CLI and to display the results.
-    name = "identity"
+    name = "Anatomical"
 
     # List of parameters for the solver. The benchmark will consider
     # the cross product for each key in the dictionary.
