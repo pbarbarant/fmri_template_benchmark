a16ba0051ec311c241b6138eca92d5446c1cb683
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-08-06 20:28:11 +0200

    Refactor Forrest dataset initialization

--- datasets/forrest.py
+++ datasets/forrest.py
@@ -15,30 +15,12 @@ class Dataset(BaseDataset):
     # Name to select the dataset in the CLI and to display the results.
     name = "Forrest"
 
-    # List of parameters to generate the datasets. The benchmark will consider
-    # the cross product for each key in the dictionary.
-    # Any parameters 'param' defined here is available as `self.param`.
-    parameters = {
-        "target": [
-            "sub-01",
-            "sub-02",
-            "sub-03",
-            "sub-04",
-            "sub-05",
-            "sub-06",
-            "sub-07",
-            "sub-08",
-            "sub-10",
-            "sub-11",
-        ],
-    }
-
     # List of packages needed to run the dataset. See the corresponding
     # section in objective.py
     install_pip = "pip"
     requirements = ["nilearn", "pandas"]
 
-    def __init__(self, target="sub-01"):
+    def __init__(self):
         self.subjects = [
             "sub-01",
             "sub-02",
@@ -80,6 +62,5 @@ class Dataset(BaseDataset):
             dict_alignment=dict_alignment,
             dict_decoding=dict_decoding,
             dict_labels=dict_labels,
-            target=self.target,
             mask=mask,
         )
