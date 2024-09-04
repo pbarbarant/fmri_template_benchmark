7453bc5c8dafb58bd683783a61fea36999226fb4
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-09-04 16:36:03 +0200

    refactor: Update Forrest dataset loading to include surface projection

--- datasets/forrest.py
+++ datasets/forrest.py
@@ -6,14 +6,16 @@ from benchopt import BaseDataset, safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     from benchmark_utils.config import DATA_PATH_FORREST, MEMORY
-    from benchmark_utils.datasets_utils import load_dataset, load_mask
+    from benchmark_utils.datasets_utils import load_dataset_surf, load_mask
     from pathlib import Path
+    from nilearn.experimental import surface
 
 
 # All datasets must be named `Dataset` and inherit from `BaseDataset`
 class Dataset(BaseDataset):
     # Name to select the dataset in the CLI and to display the results.
     name = "Forrest"
+    mesh_name = "fsaverage5"
 
     # List of packages needed to run the dataset. See the corresponding
     # section in objective.py
@@ -47,21 +49,27 @@ class Dataset(BaseDataset):
         dict_decoding = dict()
         dict_labels = dict()
         for subject in self.subjects:
+            print(f"Loading data for subject {subject}")
             (
                 data_alignment,
                 data_decoding,
                 labels,
-            ) = load_dataset(subject, data_path, mask)
+            ) = load_dataset_surf(subject, data_path, mask, self.mesh_name)
 
             dict_alignment[subject] = data_alignment
             dict_decoding[subject] = data_decoding
             dict_labels[subject] = labels
 
+        masker = surface.SurfaceMasker().fit(
+            next(iter(dict_alignment.values()))
+        )
+
         # The dictionary defines the keyword arguments for `Objective.set_data`
         return dict(
+            dataset_name=self.name,
             dict_alignment=dict_alignment,
             dict_decoding=dict_decoding,
             dict_labels=dict_labels,
-            mask=mask,
-            dataset_name=self.name,
+            masker=masker,
+            mesh_name=self.mesh_name,
         )
