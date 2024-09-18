fe7a1c9c6b7cfdef33c9694040b4b12ede8c2ea8
Pierre-Louis Barbarant <plbarbarant@mail.com>
2024-09-18 18:26:39 +0200

    Simplify Procrustes solver

--- solvers/procrustes.py
+++ solvers/procrustes.py
@@ -4,22 +4,14 @@ from benchopt import BaseSolver, safe_import_context
 # - skipping import to speed up autocompletion in CLI.
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
-    from pathlib import Path
-
     import numpy as np
-    import matplotlib.pyplot as plt
     from benchopt.stopping_criterion import SingleRunCriterion
-    from joblib import Parallel, delayed
-    from nilearn import datasets
-    from nilearn.experimental.surface._surface_image import SurfaceImage
-    from nilearn.surface import load_surf_mesh
-    from scipy import linalg
-    from sklearn.cluster import AgglomerativeClustering, KMeans
-
-    from benchmark_utils.utils import (
-        mesh_connectivity_matrix,
-        plot_surf_img,
-        LabeledImage,
+    from benchmark_utils.procrustes_utils import (
+        compute_parcellation,
+        compute_alignments,
+        compute_template,
+        plot_parcellation,
+        project,
     )
 
 
@@ -33,9 +25,10 @@ class Solver(BaseSolver):
     # the cross product for each key in the dictionary.
     # All parameters 'p' defined here are available as 'self.p'.
     parameters = {
-        "n_iter": [10],
-        "n_parcels": [100],
+        "nits": [10],
+        "n_parcels": [200],
         "clustering": ["ward"],
+        "scaling": [False],
     }
 
     # List of packages needed to run the solver. See the corresponding
@@ -62,356 +55,6 @@ class Solver(BaseSolver):
         self.masker = masker
         self.mesh_name = mesh_name
 
-    def _scaled_procrustes(self, X, Y, scaling=False, primal=None):
-        """
-        Compute a mixing matrix R and a scaling sc such that Frobenius norm
-        ||sc RX - Y||^2 is minimized and R is an orthogonal matrix
-
-        Parameters
-        ----------
-        X: (n_samples, n_features) nd array
-            source data
-        Y: (n_samples, n_features) nd array
-            target data
-        scaling: bool
-            If scaling is true, computes a floating scaling parameter
-            sc such that:
-            ||sc * RX - Y||^2 is minimized and
-            - R is an orthogonal matrix
-            - sc is a scalar
-            If scaling is false sc is set to 1
-        primal: bool or None, optional,
-            Whether the SVD is done on the YX^T (primal) or Y^TX (dual)
-            if None primal is used iff n_features <= n_timeframes
-
-        Returns
-        ----------
-        R: (n_features, n_features) nd array
-            transformation matrix
-        sc: int
-            scaling parameter
-        """
-        X = X.astype(np.float64, copy=False)
-        Y = Y.astype(np.float64, copy=False)
-        if np.linalg.norm(X) == 0 or np.linalg.norm(Y) == 0:
-            return np.eye(X.shape[1]), 1
-        if primal is None:
-            primal = X.shape[0] >= X.shape[1]
-        if primal:
-            A = Y.T.dot(X)
-            if A.shape[0] == A.shape[1]:
-                A += +1.0e-18 * np.eye(A.shape[0])
-            U, s, V = linalg.svd(A, full_matrices=0)
-            R = U.dot(V)
-        else:  # "dual" mode
-            Uy, sy, Vy = linalg.svd(Y, full_matrices=0)
-            Ux, sx, Vx = linalg.svd(X, full_matrices=0)
-            A = np.diag(sy).dot(Uy.T).dot(Ux).dot(np.diag(sx))
-            U, s, V = linalg.svd(A)
-            R = Vy.T.dot(U).dot(V).dot(Vx)
-
-        if scaling:
-            sc = s.sum() / (np.linalg.norm(X) ** 2)
-        else:
-            sc = 1
-        return R.T, sc
-
-    def _template_procrustes(
-        self, imgs, n_iter=10, scaling=False, primal=None
-    ):
-        """
-        Compute the template of a set of images using Procrustes analysis
-
-
-        Parameters
-        ----------
-        imgs: (n_subjects, n_features, n_vertices) nd array
-            set of images
-        n_iter: int, optional
-            number of iterations
-        scaling: bool, optional
-            If scaling is true, computes a floating scaling parameter
-            sc such that:
-            ||sc * RX - Y||^2 is minimized and
-            - R is an orthogonal matrix
-            - sc is a scalar
-            If scaling is false sc is set to 1
-        primal: bool or None, optional,
-            Whether the SVD is done on the YX^T (primal) or Y^TX (dual)
-            if None primal is used iff n_features <= n_timeframes
-
-        Returns
-        ----------
-        X: (n_features, n_vertices) nd array
-            template
-        R_list: list of (n_features, n_features) nd array
-            list of transformation matrices
-        sc_list: list of int
-            list of scaling parameters
-        """
-        # Initialize the template as the mean of the images
-        n_sub, _, n_vertices = imgs.shape
-        X = np.mean(imgs, axis=0)
-        R_list = [np.eye(n_vertices) for _ in range(n_sub)]
-        sc_list = [1 for _ in range(n_sub)]
-        for _ in range(n_iter):
-            for i, Y in enumerate(imgs):
-                R, sc = self._scaled_procrustes(
-                    X, Y, scaling=scaling, primal=primal
-                )
-                X = X.dot(R.T) * sc
-                R_list[i] = R
-                sc_list[i] = sc
-        return X, R_list, sc_list
-
-    def _compute_alignments(self, subject_list, n_jobs=10):
-        """
-        Compute the template and the transformation matrices
-        in parceled fashion for a set of subjects
-
-        Parameters
-        ----------
-        subject_list: list of str
-            List of subjects
-
-        Returns
-        -------
-        labels: (n_vertices,) nd array
-            parcellation
-        R_list_parcelled: list of list of rotation matrices
-            for each parcel and each subject
-        sc_list_parcelled: list of list of scaling parameters
-            for each parcel and each subject
-        """
-        imgs = np.stack(
-            [
-                self.masker.transform(self.dict_alignment[subject].img)
-                for subject in subject_list
-            ]
-        )
-
-        labels = self._compute_parcellation(
-            imgs[0],
-            clustering=self.clustering,
-            n_parcels=self.n_parcels,
-            mesh=next(iter(self.dict_alignment.values())).img.mesh,
-        )
-        unique_labels = np.unique(labels)
-        # Compute the template and the transformation matrices for each label
-        outputs = Parallel(n_jobs=n_jobs)(
-            delayed(self._template_procrustes)(imgs[..., labels == label])
-            for label in unique_labels
-        )
-        R_list_parcelled = [output[1] for output in outputs]
-        sc_list_parcelled = [output[2] for output in outputs]
-
-        return labels, R_list_parcelled, sc_list_parcelled
-
-    def _compute_parcellation(
-        self, data, mesh=None, clustering="kmeans", n_parcels=10
-    ):
-        """Compute a parcellation of the data using clustering
-
-        Parameters
-        ----------
-        data : ndarray of shape (n_samples, n_vertices)
-            Data used to compute the parcellation
-        clustering : str, optional
-            Clustering strategy, by default "kmeans"
-        n_parcels : int, optional
-            Number of parcels, by default 10
-
-        Returns
-        -------
-        labels
-            ndarray of shape (n_vertices,) containing the parcel labels of each
-            vertex
-        """
-        # Reshape the data to 2D (n_vertices, n_samples)
-        n_vertices = data.shape[1]
-        data_2d = data.reshape(n_vertices, -1)
-
-        # Choose the clustering method
-        if clustering.lower() == "kmeans":
-            clusterer = KMeans(n_clusters=n_parcels, random_state=42)
-            # Fit the clustering
-            labels = clusterer.fit_predict(data_2d).reshape(data.shape[1])
-
-            return labels
-
-        elif clustering.lower() == "ward":
-            # Compute the connectivity matrix for each hemisphere
-            coordinates_left, faces_left = load_surf_mesh(mesh.parts["left"])
-            coordinates_right, faces_right = load_surf_mesh(
-                mesh.parts["right"]
-            )
-            connectivity_left = mesh_connectivity_matrix(
-                coordinates_left, faces_left
-            )
-            connectivity_right = mesh_connectivity_matrix(
-                coordinates_right, faces_right
-            )
-            clusterer_left = AgglomerativeClustering(
-                n_clusters=n_parcels,
-                connectivity=connectivity_left,
-                linkage="ward",
-            )
-            clusterer_right = AgglomerativeClustering(
-                n_clusters=n_parcels,
-                connectivity=connectivity_right,
-                linkage="ward",
-            )
-            X_left = np.mean(data_2d[: n_vertices // 2, :], axis=1).reshape(
-                -1, 1
-            )
-            X_right = np.mean(data_2d[n_vertices // 2 :, :], axis=1).reshape(
-                -1, 1
-            )
-            labels_left = clusterer_left.fit_predict(X_left)
-            labels_right = clusterer_right.fit_predict(X_right)
-
-            return np.concatenate([labels_left, labels_right])
-        elif clustering.lower() == "destrieux":
-            if data_2d.shape[0] != 20484:
-                raise ValueError(
-                    (
-                        "The Destrieux parcellation is only available "
-                        "for the fsaverage5 mesh."
-                    )
-                )
-            destrieux = datasets.fetch_atlas_surf_destrieux()
-            labels_left = destrieux["map_left"]
-            labels_right = destrieux["map_right"]
-            return np.concatenate([labels_left, labels_right])
-        else:
-            raise ValueError(
-                (
-                    "Unsupported clustering method. Choose 'kmeans',"
-                    "'ward' or 'destrieux'."
-                )
-            )
-
-    def _project(
-        self,
-        X,
-        labels,
-        R_list,
-        sc_list,
-        n_sub,
-        classes,
-    ):
-        """Project the data of a subject onto the template
-
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_vertices)
-            Data to project
-        labels : ndarray of shape (n_vertices,)
-            Array containing the parcel labels of each vertex
-        R_list : List
-            List of list of rotation matrices for each parcel and each subject
-        sc_list : List
-            List of list of scaling parameters for each parcel and each subject
-        n_sub : int
-            Subject index
-        classes : ndarray of shape (n_samples,)
-            Array containing the class of each sample
-
-        Returns
-        -------
-        projected_data : LabeledImage
-            Projected data
-        """
-        # Decompose X into parcels
-        X_transform = np.zeros_like(X)
-        unique_labels = np.unique(labels)
-
-        for i in range(len(unique_labels)):
-            label = unique_labels[i]
-            X_reduced = X[:, labels == label]
-            X_transform[:, labels == label] = (
-                X_reduced.dot(R_list[i][n_sub].T) * sc_list[i][n_sub]
-            )
-
-        projected_img = self.masker.inverse_transform(X_transform)
-        projected_data = LabeledImage(
-            img=projected_img,
-            labels=classes,
-        )
-
-        return projected_data
-
-    def _compute_template(
-        self,
-        dict_subjects,
-        parcellation_labels,
-        R_list,
-        sc_list,
-    ):
-        """Compute the template of a set of subjects from given
-        parcellation labels, rotation matrices and scaling parameters
-
-        Parameters
-        ----------
-        dict_subjects : Dict[str, LabeledImage]
-            Dictionary containing the data of the subjects
-        parcellation_labels : ndarray of shape (n_vertices,)
-            Array containing the parcel labels of each vertex
-        R_list : List
-            List of list of rotation matrices for each parcel
-            and each subject
-        sc_list : List
-            List of list of scaling parameters for each parcel
-            and each subject
-
-        Returns
-        -------
-        _type_
-            _description_
-        """
-        subject_list = list(dict_subjects.keys())
-        template_data = np.zeros_like(
-            self.masker.transform(self.dict_decoding[subject_list[0]].img)
-        )
-        print(f"Computing the template with shape {template_data.shape}")
-        for i, subject in enumerate(subject_list):
-            projected_img = self._project(
-                self.masker.transform(self.dict_decoding[subject].img),
-                parcellation_labels,
-                R_list,
-                sc_list,
-                i,
-                None,
-            ).img
-            projected_data = self.masker.transform(projected_img)
-            template_data += projected_data / len(subject_list)
-
-        template_img = self.masker.inverse_transform(template_data)
-        template_labels = self.dict_decoding[subject_list[0]].labels
-        template = LabeledImage(img=template_img, labels=template_labels)
-        return template
-
-    def _plot_parcellation(self, labels):
-        img_labels = SurfaceImage(
-            mesh=next(iter(self.dict_alignment.values())).img.mesh,
-            data={
-                "left": labels[: len(labels) // 2],
-                "right": labels[len(labels) // 2 :],
-            },
-        )
-
-        fig = plot_surf_img(img_labels, cmap="tab20")
-        fig.suptitle(f"Parcellation of the brain in {self.n_parcels} parcels")
-        output_dir = (
-            Path(__file__).parent.parent
-            / "figures"
-            / "parcellations"
-            / f"{self.clustering}"
-        )
-        output_dir.mkdir(parents=True, exist_ok=True)
-        fig.savefig(output_dir / f"{self.n_parcels}.pdf")
-        plt.close(fig)
-
     def run(self, n_iter):
         # This is the function that is called to evaluate the solver.
         # It runs the algorithm for a given a number of iterations `n_iter`.
@@ -420,35 +63,60 @@ class Solver(BaseSolver):
 
         # Get the list of subjects
         subject_list = list(self.dict_alignment.keys())
+        mesh = next(iter(self.dict_alignment.values())).img.mesh
 
-        # Compute the Procrustes alignment
-        parcellation_labels, R_list, sc_list = self._compute_alignments(
-            list(self.dict_alignment.keys()), n_jobs=10
+        # Compute the parcellation
+        parcellation_data = np.concatenate(
+            [
+                self.masker.transform(self.dict_alignment[subject].img)
+                for subject in subject_list
+            ],
+            axis=0,
+        )
+        parcellation_labels = compute_parcellation(
+            parcellation_data,
+            clustering=self.clustering,
+            n_parcels=self.n_parcels,
+            mesh=mesh,
         )
 
         # Plot the parcellation
-        self._plot_parcellation(parcellation_labels)
+        plot_parcellation(
+            mesh=mesh,
+            labels=parcellation_labels,
+            clustering=self.clustering,
+            n_parcels=self.n_parcels,
+        )
 
-        # Get the list of subjects
-        subject_list = list(self.dict_alignment.keys())
+        # Compute the Procrustes alignment
+        R_list, sc_list = compute_alignments(
+            self.dict_alignment,
+            parcellation_labels=parcellation_labels,
+            masker=self.masker,
+            scaling=self.scaling,
+            n_iter=self.nits,
+            n_jobs=10,
+        )
 
         # Compute the barycenter
-        self.template = self._compute_template(
+        self.template = compute_template(
             self.dict_decoding,
-            parcellation_labels,
-            R_list,
-            sc_list,
+            parcellation_labels=parcellation_labels,
+            masker=self.masker,
+            R_list=R_list,
+            sc_list=sc_list,
         )
 
         # Align the data
         self.dict_aligned = {
-            subject: self._project(
-                self.masker.transform(self.dict_decoding[subject].img),
-                parcellation_labels,
-                R_list,
-                sc_list,
-                i,
-                self.dict_decoding[subject].labels,
+            subject: project(
+                self.dict_decoding[subject].img,
+                masker=self.masker,
+                labels=parcellation_labels,
+                R_list=R_list,
+                sc_list=sc_list,
+                n_sub=i,
+                classes=self.dict_decoding[subject].labels,
             )
             for i, subject in enumerate(subject_list)
         }
@@ -461,7 +129,7 @@ class Solver(BaseSolver):
         # it is customizable for each benchmark.
         solver_name = (
             self.name
-            + f"_niter_{self.n_iter}_{self.clustering}_{self.n_parcels}"
+            + f"_niter_{self.nits}_{self.clustering}_{self.n_parcels}_sc_{self.scaling}"
         )
         return dict(
             aligned_dataset=(
