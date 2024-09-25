454870bfbcf372427460a1285cbca6cec95eec46
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-09-25 22:01:05 +0200

    Refactor generate_figures.py and update figure size

--- benchmark_utils/generate_figures.py
+++ benchmark_utils/generate_figures.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scienceplots  # noqa: F401
 import seaborn as sns
-from PyPDF2 import PdfMerger
 
 plt.rcParams["figure.dpi"] = 500
 
@@ -39,7 +38,7 @@ plt.style.use(["science", "nature", "no-latex"])
 sns.set_context("paper", font_scale=1.3)
 
 # Create the figure and axes with a specific size
-fig, ax = plt.subplots(figsize=(12, 20))
+fig, ax = plt.subplots(figsize=(12, 5))
 
 # Create the box plot
 sns.boxplot(
@@ -49,20 +48,20 @@ sns.boxplot(
     hue="solver_name",
     showfliers=False,
     ax=ax,
-    # fill=False,
-    # legend=False,
-    # color="k",
+    fill=False,
+    legend=False,
+    color="k",
 )
 # Create the scatter plot
-# sns.stripplot(
-#     data=df,
-#     x="objective_cv_scores",
-#     y="data_name",
-#     size=4,
-#     hue="solver_name",
-#     dodge=True,
-#     jitter=True,
-# )
+sns.stripplot(
+    data=df,
+    x="objective_cv_scores",
+    y="data_name",
+    size=4,
+    hue="solver_name",
+    dodge=True,
+    jitter=True,
+)
 
 # Customize the plot
 ax.set_xlabel("Accuracy", fontweight="bold")
@@ -101,137 +100,99 @@ plt.show()
 
 
 # %%
-# Keep only the FUGW solvers with eps=0.01 and rho=100.0 and
-# the Procrustes and Anatomical solvers
-df_short = df[
-    df["solver_name"].str.contains("FUGW")
-    & df["solver_name"].str.contains("eps=0.01")
-    & df["solver_name"].str.contains("rho=100.0]")
-    | df["solver_name"].str.contains("Procrustes")
-    | df["solver_name"].str.contains("Anatomical")
-]
-
-# Remove the useless annotations
-df_short["solver_name"] = df_short["solver_name"].str.replace(
-    r"FUGW\[alpha=([\d.]+).*?\]", r"FUGW(alpha=\1)", regex=True
-)
-
-df_short["solver_name"] = df_short["solver_name"].str.replace(
-    r"\[.*\]", "", regex=True
-)
-# Sort alphabetically by dataset name and solver name
-df_short.sort_values(by=["solver_name", "data_name"], inplace=True)
-
-# Drop the Neuromod dataset
-df_short.drop(
-    df_short[df_short["data_name"].str.contains("Neuromod")].index,
-    inplace=True,
-)
-
-
-# Create the figure and axes with a specific size
-fig, ax = plt.subplots(figsize=(10, 6))
-
-# Create the box plot
-sns.boxplot(
-    data=df_short,
-    x="objective_cv_scores",
-    y="data_name",
-    hue="solver_name",
-    showfliers=False,
-    ax=ax,
-    fill=False,
-    legend=False,
-    color="k",
-)
-# Create the scatter plot
-sns.stripplot(
-    data=df_short,
-    x="objective_cv_scores",
-    y="data_name",
-    size=4,
-    hue="solver_name",
-    dodge=True,
-    jitter=True,
-)
-
-# Customize the plot
-ax.set_xlabel("Accuracy", fontweight="bold")
-ax.set_ylabel("Dataset", fontweight="bold")
-ax.set_title(
-    "Prediction accuracies for various template estimators",
-    fontweight="bold",
-    fontsize="large",
-)
-# Set the legend title
-ax.legend(title="Alignment method", title_fontsize="large")
-
-# Move the legend outside the plot
-sns.move_legend(ax, "center left", bbox_to_anchor=(1, 0.5))
-
-# Add gray rectangles to separate the datasets
-for i, data_name in enumerate(df_short["data_name"].unique()):
-    if i % 2 == 0:
-        ax.axhspan(i - 0.5, i + 0.5, color="gray", alpha=0.05)
-
-# Fix underscores in the dataset names
-ax.set_yticklabels(
-    [name.replace("_", " ") for name in df_short["data_name"].unique()]
-)
-
-# Adjust the layout to prevent the legend from being cut off
-plt.tight_layout()
-
-# Save the figure with high resolution
-plt.savefig(
-    figures_path / "boxplot_accuracies_summary.pdf",
-    dpi=500,
-    bbox_inches="tight",
-)
-
-
-# %%
 # Concatenate pdfs per dataset and solver
 aligned_dataset_paths = figures_path / "aligned_datasets"
 # Get the list of folders
 aligned_datasets = [
     folder for folder in aligned_dataset_paths.iterdir() if folder.is_dir()
 ]
-# Get the subfolders in one list
-solvers_paths = [
-    solver
-    for dataset in aligned_datasets
-    for solver in dataset.iterdir()
-    if solver.is_dir()
-]
+
+import matplotlib.gridspec as gridspec
+import matplotlib as mpl
+from nilearn import surface, plotting, datasets
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
-def concat_pdf(folder):
-    subject_paths = [
-        subject for subject in folder.iterdir() if subject.is_dir()
+def plot_contrast(
+    ax,
+    contrast_path,
+    hemi="left",
+    cmap="coolwarm",
+    **kwargs,
+):
+    # Load the contrast
+    surface_map = surface.load_surf_data(str(contrast_path) + f"_{hemi}.gii")
+    fsaverage = datasets.fetch_surf_fsaverage()
+    plotting.plot_surf(
+        fsaverage.infl_left,
+        surface_map,
+        cmap=cmap,
+        hemi="left",
+        axes=ax,
+        colorbar=False,
+        bg_map=fsaverage.sulc_left,
+        bg_on_data=True,
+        darkness=0.5,
+        **kwargs,
+    )
+
+
+def generate_tiling_figure(dataset_path, vmin=-10, vmax=10):
+    # Grab the available solvers
+    solvers = [solver for solver in dataset_path.iterdir() if solver.is_dir()]
+    # List the contrasts for the first solver in the template folder
+    contrasts = [
+        contrast
+        for contrast in (solvers[0] / "template").iterdir()
+        if contrast.is_file()
     ]
-    # Get the list of contrast_names
-    contrasts_paths = glob.glob(str(subject_paths[0] / "*.pdf"))
-    # Keep only the contrast names
-    contrast_names = [
-        Path(contrast_path).stem for contrast_path in contrasts_paths
-    ]
-    for contrast in contrast_names:
-        pdf_files = []
-        for subject_path in subject_paths:
-            pdf_files.append(*glob.glob(str(subject_path / f"{contrast}.pdf")))
-        # Sort the pdf_files
-        pdf_files.sort()
-        # Concatenate the pdf_files
-        merger = PdfMerger()
-        for pdf_file in pdf_files:
-            merger.append(pdf_file)
-        output_filename = folder / f"{contrast}.pdf"
-        merger.write(output_filename)
-        # Close the PdfMerger object
-        merger.close()
+    # Get the contrast names without the hemisphere
+    contrast_names = [contrast.stem.split("_")[0] for contrast in contrasts]
+    contrast_names = list(set(contrast_names))
+
+    # Create the figure and axes with a specific size
+    fig = plt.figure(figsize=(3 * len(solvers), 3 * len(contrast_names)))
+    grid_spec = gridspec.GridSpec(
+        len(contrast_names), len(solvers), figure=fig
+    )
+    for i, contrast_name in enumerate(contrast_names):
+        for j, solver in enumerate(solvers):
+            ax = fig.add_subplot(grid_spec[i, j], projection="3d")
+            plot_contrast(
+                ax,
+                solver / "template" / f"{contrast_name}",
+                cmap="coolwarm",
+                vmin=vmin,
+                vmax=vmax,
+            )
+            if i == 0:
+                ax.set_title(solver.name)
+            if j == 0:
+                # Add the contrast name in a separate column
+                ax.text2D(
+                    0.0,
+                    0.3,
+                    contrast_name,
+                    transform=ax.transAxes,
+                    fontsize=12,
+                    fontweight="bold",
+                    rotation=90,
+                )
+    # Add colorbar
+    ax = fig.add_subplot(grid_spec[len(contrast_names) // 2, :])
+    ax.axis("off")
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="2%")
+    fig.add_axes(cax)
+    fig.colorbar(
+        mpl.cm.ScalarMappable(
+            norm=mpl.colors.Normalize(vmin=vmin, vmax=vmax), cmap="coolwarm"
+        ),
+        cax=cax,
+    )
+    fig.savefig(dataset_path / "templates.pdf", dpi=500, bbox_inches="tight")
 
 
-for solver_path in solvers_paths:
-    print(f"Concatenating pdfs for {solver_path}")
-    concat_pdf(solver_path)
+for dataset_path in aligned_datasets:
+    print(f"Generating template figure for {dataset_path.name}")
+    generate_tiling_figure(dataset_path)
