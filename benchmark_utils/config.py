6eb0f009af087baea5ce0ef60531b6b09afc0045
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-10-01 17:24:10 +0200

    Add mario config

--- benchmark_utils/config.py
+++ benchmark_utils/config.py
@@ -13,14 +13,17 @@ DATA_PATH_IBC_AUDIO = root / "IBC_Audio_fsaverage5"
 # path to IBC Mathlang data
 DATA_PATH_IBC_MATHLANG = root / "IBC_MathLanguage_fsaverage5"
 
+# path to IBC FacesBody data
+DATA_PATH_IBC_FACEBODY = root / "IBC_FaceBody_fsaverage5"
+
+# path to IBC Mario data
+DATA_PATH_IBC_MARIO = root / "IBC_Mario_fsaverage5"
+
 # path to Neuromod data
 DATA_PATH_NEUROMOD = root / "Neuromod"
 
 # path to Forrest data
 DATA_PATH_FORREST = root / "Forrest"
-
-# path to IBC FacesBody data
-DATA_PATH_IBC_FACEBODY = root / "IBC_FaceBody_fsaverage5"
 
 # path to memory
 MEMORY = "/data/parietal/store3/work/pbarbara/tmp"
