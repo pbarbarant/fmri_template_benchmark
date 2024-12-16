62d43daa973e0e921105b1efb16a6cf324c189c7
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-12-16 18:20:40 +0100

    Refactor Solver class in procrustes.py to integrate TemplateAlignment for image alignment and streamline template generation

--- solvers/procrustes.py
+++ solvers/procrustes.py
@@ -5,17 +5,9 @@ from benchopt import BaseSolver, safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
-    from joblib import Parallel, delayed
-    from functools import partial
+    from fmralign.template_alignment import TemplateAlignment
     from benchopt.stopping_criterion import SingleRunCriterion
-    from benchmark_utils.procrustes_utils import (
-        rescaled_euclidean_mean,
-        align_images_to_template,
-        compute_parcellation,
-        compute_template,
-        plot_parcellation,
-        project,
-    )
+    from benchmark_utils.utils import LabeledImage
 
 
 # The benchmark solvers must be named `Solver` and
@@ -28,7 +20,9 @@ class Solver(BaseSolver):
     # the cross product for each key in the dictionary.
     # All parameters 'p' defined here are available as 'self.p'.
     parameters = {
-        "n_parcels": [100,],
+        "n_parcels": [
+            3,
+        ],
         "clustering": ["ward"],
         "scaling": [True],
     }
@@ -44,8 +38,6 @@ class Solver(BaseSolver):
         self,
         dict_alignment,
         dict_decoding,
-        masker,
-        mesh_name,
     ):
         # Define the information received by each solver from the objective.
         # The arguments of this function are the results of the
@@ -54,108 +46,7 @@ class Solver(BaseSolver):
         # It is customizable for each benchmark.
         self.dict_alignment = dict_alignment
         self.dict_decoding = dict_decoding
-        self.masker = masker
-        self.mesh_name = mesh_name
-
-    def template_procrustes(self, imgs, n_iter=2, scaling=False, primal=None):
-        """
-        Compute the template of a set of images using Procrustes analysis
-
-
-        Parameters
-        ----------
-        imgs: (n_subjects, n_features, n_vertices) nd array
-            set of images
-        n_iter: int, optional
-            number of iterations
-        scaling: bool, optional
-            If scaling is true, computes a floating scaling parameter
-            sc such that:
-            ||sc * RX - Y||^2 is minimized and
-            - R is an orthogonal matrix
-            - sc is a scalar
-            If scaling is false sc is set to 1
-        primal: bool or None, optional,
-            Whether the SVD is done on the YX^T (primal) or Y^TX (dual)
-            if None primal is used iff n_features <= n_timeframes
-
-        Returns
-        ----------
-        X: (n_features, n_vertices) nd array
-            template
-        R_list: list of (n_features, n_features) nd array
-            list of transformation matrices
-        sc_list: list of int
-            list of scaling parameters
-        """
-        aligned_imgs = imgs
-        for _ in range(n_iter):
-            template = rescaled_euclidean_mean(aligned_imgs, scaling)
-            aligned_imgs, R_list, sc_list = align_images_to_template(
-                imgs,
-                template,
-            )
-        return template, R_list, sc_list
-
-    def compute_alignments(
-        self,
-        dict_subjects,
-        parcellation_labels,
-        masker,
-        scaling=False,
-        n_iter=2,
-        n_jobs=10,
-    ):
-        """
-        Compute the template and the transformation matrices
-        in parceled fashion for a set of subjects
-
-        Parameters
-        ----------
-        Dict_subjects: Dict[str, LabeledImage]
-            Dictionary containing the data of the subjects
-        masker: NiftiMasker
-            Masker used to transform the data
-        parcellation_labels: ndarray of shape (n_vertices,)
-            Array containing the parcel labels of each vertex
-        scaling: bool, optional
-            Compute a scaling parameter, by default False
-        n_iter: int, optional
-            Number of iterations, by default 10
-        n_jobs: int, optional
-            Number of jobs to run in parallel, by default 10
-
-        Returns
-        -------
-        R_list_parcelled: list of list of rotation matrices
-            for each parcel and each subject
-        sc_list_parcelled: list of list of scaling parameters
-            for each parcel and each subject
-        """
-        subject_list = list(dict_subjects.keys())
-        imgs = np.stack(
-            [
-                masker.transform(dict_subjects[subject].img)
-                for subject in subject_list
-            ],
-        )
-        unique_labels = np.unique(parcellation_labels)
-        # Compute the template and the transformation matrices for each label
-        template_procrustes_partial = partial(
-            self.template_procrustes,
-            n_iter=n_iter,
-            scaling=scaling,
-            primal=None,
-        )
-        outputs = Parallel(n_jobs=n_jobs)(
-            delayed(template_procrustes_partial)(
-                imgs[..., parcellation_labels == label]
-            )
-            for label in unique_labels
-        )
-        R_list_parcelled = [output[1] for output in outputs]
-        sc_list_parcelled = [output[2] for output in outputs]
-        return R_list_parcelled, sc_list_parcelled
+        self.dict_aligned = dict()
 
     def run(self, n_iter):
         # This is the function that is called to evaluate the solver.
@@ -165,62 +56,44 @@ class Solver(BaseSolver):
 
         # Get the list of subjects
         subject_list = list(self.dict_alignment.keys())
-        mesh = next(iter(self.dict_alignment.values())).img.mesh
 
-        # Compute the parcellation
-        parcellation_data = np.concatenate(
-            [
-                self.masker.transform(self.dict_alignment[subject].img)
-                for subject in subject_list
-            ],
-            axis=0,
-        )
-        parcellation_labels = compute_parcellation(
-            parcellation_data,
+        # Get the list of images
+        imgs = [self.dict_alignment[subject].img for subject in subject_list]
+
+        # Align the images
+        algo = TemplateAlignment(
+            alignment_method="scaled_orthogonal",
+            n_pieces=self.n_parcels,
             clustering=self.clustering,
-            n_parcels=self.n_parcels,
-            mesh=mesh,
+            # scaling=self.scaling,
         )
+        algo.fit(imgs)
 
-        # Plot the parcellation
-        plot_parcellation(
-            mesh=mesh,
-            labels=parcellation_labels,
-            clustering=self.clustering,
-            n_parcels=self.n_parcels,
+        # Retrieve the parcellation, masker
+        self.labels, self.parcellation_img = algo.get_parcellation()
+        self.masker = algo.masker
+
+        # Align the images
+        template_data = np.zeros_like(
+            self.masker.transform(self.dict_decoding[subject_list[0]].img)
         )
-
-        # Compute the Procrustes alignment
-        R_list, sc_list = self.compute_alignments(
-            dict_subjects=self.dict_alignment,
-            parcellation_labels=parcellation_labels,
-            masker=self.masker,
-            scaling=self.scaling,
-            n_jobs=10,
-        )
-
-        # Compute the barycenter
-        self.template = compute_template(
-            self.dict_decoding,
-            parcellation_labels=parcellation_labels,
-            masker=self.masker,
-            R_list=R_list,
-            sc_list=sc_list,
-        )
-
-        # Align the data
-        self.dict_aligned = {
-            subject: project(
-                self.dict_decoding[subject].img,
-                masker=self.masker,
-                labels=parcellation_labels,
-                R_list=R_list,
-                sc_list=sc_list,
-                n_sub=i,
-                classes=self.dict_decoding[subject].labels,
+        for i, subject in enumerate(subject_list):
+            transformed_img = algo.transform(
+                self.dict_decoding[subject].img, subject_index=i
             )
-            for i, subject in enumerate(subject_list)
-        }
+            self.dict_aligned[subject] = LabeledImage(
+                img=transformed_img,
+                y=self.dict_decoding[subject].y,
+            )
+            template_data += self.masker.transform(transformed_img) / len(
+                subject_list
+            )
+
+        # Generate the template
+        self.template = LabeledImage(
+            img=self.masker.inverse_transform(template_data),
+            y=self.dict_decoding[subject_list[0]].y,
+        )
 
     def get_result(self):
         # Return the result from one optimization run.
@@ -234,6 +107,9 @@ class Solver(BaseSolver):
         )
         return dict(
             aligned_dataset=(
+                self.parcellation_img,
+                self.labels,
+                self.masker,
                 self.template,
                 self.dict_aligned,
                 solver_name,
