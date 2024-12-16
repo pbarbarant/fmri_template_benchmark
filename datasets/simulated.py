22365b409fcab6d2595e7b0fdd352fc95965821a
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-12-16 16:01:41 +0100

    Refactor Dataset class in simulated.py to simplify data sampling and update dataset naming convention

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -5,17 +5,14 @@ from benchopt import BaseDataset, safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
-    from nilearn.experimental import surface
-    from nilearn.experimental.surface._datasets import load_fsaverage
-    from nilearn.experimental.surface._surface_image import SurfaceImage
+    from nilearn._utils.data_gen import generate_random_img
     from benchmark_utils.utils import LabeledImage
 
 
 # All datasets must be named `Dataset` and inherit from `BaseDataset`
 class Dataset(BaseDataset):
     # Name to select the dataset in the CLI and to display the results.
-    name = "Simulated"
-    mesh_name = "fsaverage5"
+    dataset_name = "Simulated"
 
     # List of packages needed to run the dataset. See the corresponding
     # section in objective.py
@@ -29,26 +26,6 @@ class Dataset(BaseDataset):
         self.n_samples_alignement = 200
         self.n_samples_decoding = 150
 
-    def _sample_data_hemi(self, n_samples, n_vertices):
-        return np.random.randn(n_samples, n_vertices)
-
-    def _sample_data_subject(self, n_samples, n_vertices):
-        left_data = self._sample_data_hemi(n_samples, n_vertices)
-        right_data = self._sample_data_hemi(n_samples, n_vertices)
-        return left_data, right_data
-
-    def _sample_surface_image(self, mesh, n_samples, n_vertices):
-        left_data, right_data = self._sample_data_subject(
-            n_samples, n_vertices
-        )
-        return SurfaceImage(
-            mesh=mesh,
-            data={
-                "left": left_data,
-                "right": right_data,
-            },
-        )
-
     def _sample_labels(self, n_samples):
         return (
             np.arange(3)
@@ -57,44 +34,33 @@ class Dataset(BaseDataset):
             .flatten()
         )
 
-    def _sample_labeled_image(self, mesh, n_samples, n_vertices):
-        img = self._sample_surface_image(mesh, n_samples, n_vertices)
-        labels = self._sample_labels(n_samples)
+    def _sample_labeled_image(self, n_samples):
+        img = generate_random_img((8, 7, 6, n_samples))
+        y = self._sample_labels(n_samples)
         return LabeledImage(
             img=img,
-            labels=labels,
+            y=y,
         )
 
     def get_data(self):
         # The return arguments of this function are passed as keyword arguments
         # to `Objective.set_data`. This defines the benchmark's
         # API to pass data. It is customizable for each benchmark.
-
-        # Create a masker to extract the data from the brain volume.
-        mesh = load_fsaverage(self.mesh_name)["pial"]
-        n_vertices = mesh.n_vertices // 2
-
         dict_alignment = dict()
         dict_decoding = dict()
 
         for subject in self.subjects:
             # Generate random surface images for each subject.
             dict_alignment[subject] = self._sample_labeled_image(
-                mesh, self.n_samples_alignement, n_vertices
+                self.n_samples_alignement
             )
             dict_decoding[subject] = self._sample_labeled_image(
-                mesh, self.n_samples_decoding, n_vertices
+                self.n_samples_decoding
             )
-
-        # Get the first image to create the masker
-        masker_img = next(iter(dict_alignment.values())).img
-        masker = surface.SurfaceMasker().fit(masker_img)
 
         # The dictionary defines the keyword arguments for `Objective.set_data`
         return dict(
-            dataset_name=self.name,
+            dataset_name=self.dataset_name,
             dict_alignment=dict_alignment,
             dict_decoding=dict_decoding,
-            masker=masker,
-            mesh_name=self.mesh_name,
         )
