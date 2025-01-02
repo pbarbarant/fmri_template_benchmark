5035113bc20491603856a07ab2b01936ec311833
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-01-02 19:02:28 +0100

    Refactor Dataset class in simulated.py to integrate NiftiMasker for improved image sampling and reduce sample sizes

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -5,8 +5,10 @@ from benchopt import BaseDataset, safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
-    from nilearn._utils.data_gen import generate_random_img
-    from benchmark_utils.utils import LabeledImage
+    from nilearn.maskers import NiftiMasker
+    from nilearn.datasets import load_mni152_brain_mask
+    from nilearn.image import concat_imgs
+    from benchmark_utils.utils import LabeledImage, Fold
 
 
 # All datasets must be named `Dataset` and inherit from `BaseDataset`
@@ -23,8 +25,8 @@ class Dataset(BaseDataset):
         self,
     ):
         self.subjects = ["sub-01", "sub-02", "sub-03"]
-        self.n_samples_alignement = 200
-        self.n_samples_decoding = 150
+        self.n_samples_alignement = 20
+        self.n_samples_decoding = 15
 
     def _sample_labels(self, n_samples):
         return (
@@ -34,33 +36,57 @@ class Dataset(BaseDataset):
             .flatten()
         )
 
-    def _sample_labeled_image(self, n_samples):
-        img, _ = generate_random_img((8, 7, 6, n_samples))
+    def _sample_labeled_image(self, n_samples, masker):
+        mask_img = masker.mask_img_
+        img = concat_imgs([mask_img] * n_samples)
         y = self._sample_labels(n_samples)
         return LabeledImage(
             img=img,
             y=y,
         )
 
+    def _sample_fold(
+        self, name, subjects, n_samples_alignement, n_samples_decoding
+    ):
+        print(f"Generating fold {name}")
+        dict_alignment = dict()
+        dict_decoding = dict()
+        mask_img = load_mni152_brain_mask(resolution=1)
+        self.masker = NiftiMasker(
+            mask_img=mask_img, memory="nilearn_cache", memory_level=1
+        ).fit()
+        for subject in subjects:
+            # Generate random surface images for each subject.
+            dict_alignment[subject] = self._sample_labeled_image(
+                n_samples_alignement, self.masker
+            )
+            dict_decoding[subject] = self._sample_labeled_image(
+                n_samples_decoding, self.masker
+            )
+
+        return Fold(
+            name=name,
+            dict_alignment=dict_alignment,
+            dict_decoding=dict_decoding,
+        )
+
     def get_data(self):
         # The return arguments of this function are passed as keyword arguments
         # to `Objective.set_data`. This defines the benchmark's
         # API to pass data. It is customizable for each benchmark.
-        dict_alignment = dict()
-        dict_decoding = dict()
-
-        for subject in self.subjects:
-            # Generate random surface images for each subject.
-            dict_alignment[subject] = self._sample_labeled_image(
-                self.n_samples_alignement
-            )
-            dict_decoding[subject] = self._sample_labeled_image(
-                self.n_samples_decoding
-            )
 
         # The dictionary defines the keyword arguments for `Objective.set_data`
+        folds = [
+            self._sample_fold(
+                f"fold-{i:02d}",
+                self.subjects,
+                self.n_samples_alignement,
+                self.n_samples_decoding,
+            )
+            for i in range(2)
+        ]
         return dict(
             dataset_name=self.name,
-            dict_alignment=dict_alignment,
-            dict_decoding=dict_decoding,
+            folds=folds,
+            masker=self.masker,
         )
