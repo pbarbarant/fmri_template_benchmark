8a269840f4cd483e0bd59bf15ff97e98090fbe64
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-01-07 16:07:09 +0100

    Refactor dataset initialization in simulated.py to enhance clarity and utilize new utility functions

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -4,11 +4,13 @@ from benchopt import BaseDataset, safe_import_context
 # - skipping import to speed up autocompletion in CLI.
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
-    from benchmark_utils.datasets_utils import (
-        sample_fold,
-    )
     from nilearn.maskers import NiftiMasker
     from fmralign.tests.utils import random_niimg
+    from benchmark_utils.datasets_utils import (
+        check_init_dataset,
+        log_dataset_info,
+        sample_dataset,
+    )
 
 
 # All datasets must be named `Dataset` and inherit from `BaseDataset`
@@ -37,20 +39,15 @@ class Dataset(BaseDataset):
         _, mask_img = random_niimg((8, 7, 6))
         masker = NiftiMasker(mask_img=mask_img).fit()
 
-        folds = [
-            sample_fold(
-                f"fold-{i:02d}",
-                masker,
-                self.subjects,
-                self.n_samples_alignement,
-                self.n_samples_decoding,
-            )
-            for i in range(2)
-        ]
-
-        return dict(
-            dataset_name=self.name,
-            folds=folds,
+        self.dataset = sample_dataset(
+            name=self.name,
             masker=masker,
-            clustering_img=mask_img,
+            subjects=self.subjects,
+            n_samples_alignement=self.n_samples_alignement,
+            n_samples_decoding=self.n_samples_decoding,
         )
+
+        check_init_dataset(self.dataset)
+        log_dataset_info(self.dataset)
+
+        return dict(dataset=self.dataset)
