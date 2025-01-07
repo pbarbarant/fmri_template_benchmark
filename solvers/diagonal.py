bae41dcef9f0857b5ffe952e9a1b8da5a749b20f
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-01-07 16:07:03 +0100

    Refactor Solver class to utilize dataset object for improved data handling and clarity

--- solvers/diagonal.py
+++ solvers/diagonal.py
@@ -5,8 +5,7 @@ from benchopt import BaseSolver, safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     from benchopt.stopping_criterion import SingleRunCriterion
-    from benchmark_utils.utils import DecodingFold
-    from benchmark_utils.solver_utils import _compute_template_one_fold
+    from benchmark_utils.solver_utils import compute_template
     from fmralign.template_alignment import TemplateAlignment
 
 
@@ -30,18 +29,14 @@ class Solver(BaseSolver):
 
     def set_objective(
         self,
-        folds,
-        masker,
-        clustering_img,
+        dataset,
     ):
         # Define the information received by each solver from the objective.
         # The arguments of this function are the results of the
         # `Objective.get_objective`. This defines the benchmark's API for
         # passing the objective to the solver.
         # It is customizable for each benchmark.
-        self.folds = folds
-        self.masker = masker
-        self.clustering_img = clustering_img
+        self.dataset = dataset
 
     def run(self, n_iter):
         # This is the function that is called to evaluate the solver.
@@ -50,26 +45,14 @@ class Solver(BaseSolver):
         # https://benchopt.github.io/performance_curves.html
         algo = TemplateAlignment(
             alignment_method="diagonal",
-            mask=self.masker,
-            clustering=self.clustering_img,
+            mask=self.dataset.masker,
+            clustering=self.dataset.clustering_img,
         )
 
-        decoding_folds = []
-        for fold in self.folds:
-            print(f"Running {self.name} solver on fold {fold.name}")
-            template, dict_aligned = _compute_template_one_fold(
-                algo,
-                fold.dict_alignment,
-                fold.dict_decoding,
-                self.masker,
-            )
-
-            decoding_fold = DecodingFold(
-                name=fold.name, template=template, dict_aligned=dict_aligned
-            )
-            decoding_folds.append(decoding_fold)
-
-        self.decoding_folds = decoding_folds
+        self.dataset = compute_template(
+            algo=algo,
+            dataset=self.dataset,
+        )
 
     def get_result(self):
         # Return the result from one optimization run.
@@ -77,6 +60,4 @@ class Solver(BaseSolver):
         # keyword arguments for `Objective.evaluate_result`
         # This defines the benchmark's API for solvers' results.
         # it is customizable for each benchmark.
-        return dict(
-            decoding_folds=self.decoding_folds,
-        )
+        return dict(dataset=self.dataset)
