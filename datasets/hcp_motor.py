b559701481636f8eccf5e4eb4264751ca9f2a159
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-01-14 01:51:20 +0100

    Add hcp motor

---
+++ datasets/hcp_motor.py
@@ -0,0 +1,45 @@
+from benchopt import BaseDataset, safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from benchmark_utils.datasets_utils import (
+        check_init_dataset,
+        fetch_hcp,
+        log_dataset_info,
+    )
+
+
+# All datasets must be named `Dataset` and inherit from `BaseDataset`
+class Dataset(BaseDataset):
+    # Name to select the dataset in the CLI and to display the results.
+    name = "HCP_Motor"
+
+    # List of packages needed to run the dataset. See the corresponding
+    # section in objective.py
+    install_pip = "pip"
+    requirements = []
+
+    def __init__(self):
+        self.subjects = 3
+        self.n_parcels = 400
+
+    def get_data(self):
+        # The return arguments of this function are passed as keyword arguments
+        # to `Objective.set_data`. This defines the benchmark's
+        # API to pass data. It is customizable for each benchmark.
+
+        # The dictionary defines the keyword arguments for `Objective.set_data`
+
+        self.dataset = fetch_hcp(
+            name=self.name,
+            subjects=int(self.subjects),
+            task="MOTOR",
+            n_parcels=self.n_parcels,
+        )
+
+        check_init_dataset(self.dataset)
+        log_dataset_info(self.dataset)
+
+        return dict(dataset=self.dataset)
