d62155123bbddbc36cbcfab3c698082a2dfe5b4e
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-01-15 19:25:01 +0100

    Add movie watching classification to decoding

--- benchmark_utils/decoding_utils.py
+++ benchmark_utils/decoding_utils.py
@@ -8,6 +8,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 from nilearn.maskers import NiftiLabelsMasker
+from nilearn import image
 
 
 def compute_groups(subject_dict):
@@ -29,10 +30,7 @@ def compute_batched_groups(subject_dict, n_groups=10):
 def compute_X_y(subject_dict, masker):
     subject_list = list(subject_dict.keys())
     X = np.concatenate(
-        [
-            masker.transform(subject_dict[subject].img)
-            for subject in subject_list
-        ]
+        [masker.transform(subject_dict[subject].img) for subject in subject_list]
     )
     y = np.concatenate([subject_dict[subject].y for subject in subject_list])
     return X, y
@@ -48,9 +46,7 @@ def compute_pearson_corrs(dataset):
     pearson_corrs = []
     for subject in dataset.subjects:
         subject_img = dataset.dict_aligned[subject].img
-        subject_corr = pearson_corr_parcels(
-            subject_img, template_img, labels_masker
-        )
+        subject_corr = pearson_corr_parcels(subject_img, template_img, labels_masker)
         pearson_corrs.append(subject_corr)
     return pearson_corrs
 
@@ -66,7 +62,7 @@ def pearson_corr_parcels(img1, img2, labels_masker):
     )
 
 
-def evaluate_dataset(dataset, max_iter=100):
+def evaluate_task_dataset(dataset, max_iter=100):
     dict_aligned = dataset.dict_aligned
     masker = dataset.masker
 
@@ -110,3 +106,88 @@ def evaluate_dataset(dataset, max_iter=100):
     print(f"Chance level: {chance_level:.2f}")
 
     return avg_score, chance_level, cv_scores_svc, pearson_corrs
+
+
+def classify_subject_movie(template_img, img, y, labels_masker):
+    segments_ids = np.unique(y)
+    res = []
+    # Get the list of segments for the template
+    segments_template = []
+    segments_subject = []
+    for segment_id in segments_ids:
+        # Get the slice of indices corresponding to the segment
+        segment_slice = y == segment_id
+        # Get the image segment for the template
+        segments_template.append(image.index_img(template_img, segment_slice))
+        # Get the image segment for the subject
+        segments_subject.append(image.index_img(img, segment_slice))
+
+    for i in range(len(segments_ids)):
+        # For each movie segment of the subject
+        segment_subject = segments_subject[i]
+        correct_id = segments_ids[i]
+        corr_list = []
+        for j in range(len(segment_id)):
+            segment_template = segments_template[j]
+            # Compute the Pearson correlation with each segment of the template
+            corr_list.append(
+                pearson_corr_parcels(segment_template, segment_subject, labels_masker)
+            )
+        # Predict the segment with the highest correlation
+        predicted_id = segments_ids[np.argmax(corr_list)]
+        res.append(predicted_id == correct_id)
+    return np.mean(res)
+
+
+def evaluate_movie_dataset(dataset):
+    dict_aligned = dataset.dict_aligned
+    masker = dataset.masker
+
+    # Compute the voxel-wise pearson correlation between all subjects
+    # and the template
+    pearson_corrs = compute_pearson_corrs(dataset)
+
+    # Create cross-validation object on each subject
+    X, y = compute_X_y(dict_aligned, masker)
+    groups = compute_groups(dict_aligned)
+
+    labels_masker = NiftiLabelsMasker(
+        labels_img=dataset.clustering_img, mask_img=masker.mask_img_
+    ).fit()
+    cv_scores_classif = []
+    for subject in dataset.subjects:
+        print(f"Subject {subject}")
+        cv_scores_classif.append(
+            classify_subject_movie(
+                template_img=dataset.template.img,
+                img=dict_aligned[subject].img,
+                y=dict_aligned[subject].y,
+                labels_masker=labels_masker,
+            )
+        )
+
+    cv_scores_dummy = cross_val_score(
+        DummyClassifier(strategy="most_frequent"),
+        X,
+        y,
+        groups=groups,
+        cv=LeaveOneGroupOut(),
+        n_jobs=-1,
+    )
+
+    avg_score = np.mean(cv_scores_classif)
+    chance_level = np.mean(cv_scores_dummy)
+
+    print(f"Average decoding accuracy: {avg_score:.2f}")
+    print(f"Chance level: {chance_level:.2f}")
+
+    return avg_score, chance_level, cv_scores_classif, pearson_corrs
+
+
+def evaluate_dataset(dataset, max_iter=100):
+    if True:
+        return evaluate_movie_dataset(dataset)
+    # if dataset.name.lower().startswith("budapest"):
+    #     return evaluate_movie_dataset(dataset)
+    # else:
+    #     return evaluate_task_dataset(dataset, max_iter=max_iter)
