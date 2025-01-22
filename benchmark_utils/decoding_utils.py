bca4f914e00a3608cb76a235b905bf07af8df383
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-01-22 13:33:52 +0100

    Add support for movie datasets in evaluate_dataset function

--- benchmark_utils/decoding_utils.py
+++ benchmark_utils/decoding_utils.py
@@ -211,9 +211,11 @@ def evaluate_movie_dataset(dataset):
 
 
 def evaluate_dataset(dataset, solver_name):
-    if dataset.name.lower().startswith(
-        "budapest"
-    ) or dataset.name.lower().startswith("raiders"):
+    if (
+        dataset.name.lower().startswith("budapest")
+        or dataset.name.lower().startswith("raiders")
+        or dataset.name.lower().endswith("movie")
+    ):
         avg_score, chance_level, cv_scores_classif, pearson_corrs = (
             evaluate_movie_dataset(dataset)
         )
