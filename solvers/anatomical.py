23b557de5142c27778863bc96b250fd017303e16
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-02-04 16:43:00 +0100

    Remove solver_name from results returned by Solver classes

--- solvers/anatomical.py
+++ solvers/anatomical.py
@@ -75,4 +75,4 @@ class Solver(BaseSolver):
         # keyword arguments for `Objective.evaluate_result`
         # This defines the benchmark's API for solvers' results.
         # it is customizable for each benchmark.
-        return dict(dataset=self.dataset, solver_name=self.name)
+        return dict(dataset=self.dataset)
