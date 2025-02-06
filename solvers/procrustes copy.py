21c8c04264141aaeb8d014bf7ecaca84c8b9348b
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-02-06 13:47:15 +0100

    Add Procrustes solver implementation for optimal transport alignment

---
+++ solvers/procrustes copy.py
@@ -0,0 +1,78 @@
+from benchopt import BaseSolver, safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from benchopt.stopping_criterion import SingleRunCriterion
+    from fmralign.template_alignment import TemplateAlignment
+    from fmralign.pairwise_alignment import PairwiseAlignment
+
+    from benchmark_utils.conf import N_JOBS
+    from benchmark_utils.solver_utils import compute_alignment
+
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(BaseSolver):
+    # Name to select the solver in the CLI and to display the results.
+    name = "ot"
+
+    # List of parameters for the solver. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    parameters = {}
+
+    # List of packages needed to run the solver. See the corresponding
+    # section in objective.py
+    install_pip = "pip"
+    requirements = []
+
+    stopping_criterion = SingleRunCriterion()
+
+    def set_objective(
+        self,
+        dataset,
+    ):
+        # Define the information received by each solver from the objective.
+        # The arguments of this function are the results of the
+        # `Objective.get_objective`. This defines the benchmark's API for
+        # passing the objective to the solver.
+        # It is customizable for each benchmark.
+        self.dataset = dataset
+
+    def run(self, n_iter):
+        # This is the function that is called to evaluate the solver.
+        # It runs the algorithm for a given a number of iterations `n_iter`.
+        # You can also use a `tolerance` or a `callback`, as described in
+        # https://benchopt.github.io/performance_curves.html
+        if self.dataset.target == "template":
+            algo = TemplateAlignment(
+                alignment_method="optimal_transport",
+                mask=self.dataset.masker,
+                clustering=self.dataset.clustering_img,
+                n_jobs=N_JOBS,
+                verbose=11,
+            )
+        else:
+            algo = PairwiseAlignment(
+                alignment_method="optimal_transport",
+                mask=self.dataset.masker,
+                clustering=self.dataset.clustering_img,
+                n_jobs=N_JOBS,
+                verbose=11,
+            )
+
+        self.dataset = compute_alignment(
+            algo=algo,
+            dataset=self.dataset,
+            solver_name=self.name,
+        )
+
+    def get_result(self):
+        # Return the result from one optimization run.
+        # The outputs of this function is a dictionary which defines the
+        # keyword arguments for `Objective.evaluate_result`
+        # This defines the benchmark's API for solvers' results.
+        # it is customizable for each benchmark.
+        return dict(dataset=self.dataset)
