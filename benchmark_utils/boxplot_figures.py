7236a99cdfa015d643e2f6d72ca97139ccc8b33f
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-08-05 09:43:47 +0200

    Remove PCA in solver utils

--- benchmark_utils/boxplot_figures.py
+++ benchmark_utils/boxplot_figures.py
@@ -14,7 +14,7 @@ data_path = Path(__file__).parent.parent / "outputs"
 figures_path = data_path.parent / "outputs" / "figures"
 figures_path.mkdir(parents=True, exist_ok=True)
 
-N_PARCELS = 200
+N_PARCELS = 400
 
 
 def get_results_dataframe(
@@ -28,81 +28,49 @@ def get_results_dataframe(
     res_list = []
     for path in results_paths:
         path = Path(path)
-        dataset = path.parent.parent.parent.name
-        solver = path.parent.parent.name
-        target = path.parent.name
+        solver = path.parent.name
         results = load(path)
-        # Add the dataset name and solver name to the results
-        results["data_name"] = dataset
+        # Add the solver name to the results
         results["solver_name"] = solver
-        results["target"] = target
         res_list.append(results)
 
     df = pd.DataFrame(res_list)
 
     # Remove the simulated data
-    df = df[~df["data_name"].str.contains("Simulated")]
-    
+    df = df[~df["dataset_name"].str.contains("Simulated")]
+    df = df[~df["dataset_name"].str.contains("Surf")]
+    df = df[~df["solver_name"].str.contains("Ridge")]
+
     # Keep only the results for the specified number of parcels
-    df = df[df["data_name"].str.contains(f"{n_parcels}")]
+    # df = df[df["dataset_name"].str.contains(f"{n_parcels}")]
 
     # Remove parcels numbers from the dataset names
-    df["data_name"] = df["data_name"].str.replace(
-        r"_[0-9]+$", "", regex=True
-    )
+    df["dataset_name"] = df["dataset_name"].str.replace(r"_[0-9]+$", "", regex=True)
 
     # For anatomical keep only the template target
     df = df[
-        ~((df["solver_name"] == "Anatomical") & (df["target"] == "template"))
+        ~((df["solver_name"] == "Anatomical") & (df["external_template"] == "True"))
     ]
-
-    # Remove the "IBC " prefix on the dataset names
-    df["data_name"] = df["data_name"].str.replace("IBC", "")
-
-    # Add a type column to indicate tasks or movie
-    df["type"] = df["data_name"].apply(
-        lambda x: "movie"
-        if x.lower().startswith("raiders") or x.lower().startswith("budapest")
-        else "task"
-    )
-
-    # For datasets split in runs, remove the "_run-0*" suffix
-    df["data_name"] = df["data_name"].str.replace(r"_run-\d+", "", regex=True)
-
-    # Add (template) to the solver name if target is template
+    
+    # Add external template information in the solver name
     df["solver_name"] = df.apply(
-        lambda x: x["solver_name"] + " (template)"
-        if x["target"] == "template"
-        else x["solver_name"] + " (pairwise)",
+        lambda x: f"{x['solver_name']} ({'Out-of-sample' if (x['external_template'] == 'True')  else 'In-sample'})",
         axis=1,
     )
 
-    # Remove the (template) suffix for the anatomical alignment
-    df["solver_name"] = df["solver_name"].str.replace(
-        "Anatomical (pairwise)", "Anatomical"
-    )
-
-    # Rename ot by Optimal Transport
-    df["solver_name"] = df["solver_name"].str.replace(
-        "ot", "Optimal Transport"
-    )
-
     # Rename Wm by WM
-    df["data_name"] = df["data_name"].str.replace("Wm", "WM")
+    # df["task_name"] = df["task_name"].str.replace("Wm", "WM")
 
     # Sort alphabetically by dataset name and solver name
-    df.sort_values(by=["data_name", "solver_name"], inplace=True)
+    df.sort_values(by=["task_name", "solver_name"], inplace=True)
 
     # Fix underscores in the solver names
     df["solver_name"] = df["solver_name"].str.replace("_", " ")
 
-    # Expand the lists in df[score]
-    df = df.explode(score)
-
     return df
 
 
-df = get_results_dataframe(data_path, score="cv_scores_classif", n_parcels=N_PARCELS)
+df = get_results_dataframe(data_path)
 
 # Set the style and font scale for better readability
 plt.style.use(["science", "nature", "no-latex"])
@@ -115,28 +83,28 @@ def create_accuracy_plot(data, figsize=(12, 7)):
     shifted_palette = original_palette[1:] + original_palette[:1]
     sns.barplot(
         data=data,
-        x="cv_scores_classif",
-        y="data_name",
+        y="score",
+        x="task_name",
         hue="solver_name",
         ax=ax,
         palette=shifted_palette,
     )
 
-    ax.set_xlabel("Accuracy", fontweight="bold")
-    ax.set_ylabel("Dataset", fontweight="bold")
+    ax.set_xlabel("Task", fontweight="bold")
+    ax.set_ylabel("Accuracy", fontweight="bold")
 
-    for i in range(len((data["data_name"].unique()))):
+    for i in range(len((data["task_name"].unique()))):
         if i % 2 == 0:
             ax.axhspan(i - 0.5, i + 0.5, color="gray", alpha=0.05)
 
     # Add a vertical line at chance level
     chance_levels = (
-        data.groupby("data_name")["chance_level"]
+        data.groupby("task_name")["chance_level"]
         .unique()
         .apply(lambda x: x[0])
     ).to_list()
     for i, chance_level in enumerate(chance_levels):
-        ax.vlines(
+        ax.hlines(
             chance_level,
             i - 0.5,
             i + 0.5,
@@ -144,10 +112,8 @@ def create_accuracy_plot(data, figsize=(12, 7)):
             linestyles="--",
             alpha=0.5,
         )
-
-    ax.set_yticklabels(
-        [name.replace("_", " ") for name in data["data_name"].unique()]
-    )
+        
+    ax.set_ylim(0, 1.05)
     ax.legend(title="Alignment method", title_fontsize="large")
     sns.move_legend(ax, "center left", bbox_to_anchor=(1, 0.5))
 
@@ -155,87 +121,10 @@ def create_accuracy_plot(data, figsize=(12, 7)):
     return fig
 
 
-# Create separate plots
-# movie_data = df[df["type"] == "movie"]
-task_data = df[df["type"] == "task"]
 
-# fig1 = create_accuracy_plot(
-#     movie_data,
-#     "Movie Prediction Accuracies",
-# )
-fig2 = create_accuracy_plot(task_data, figsize=(3.15, 8))
+fig = create_accuracy_plot(df, figsize=(11, 3))
 # fig1.savefig(figures_path / "boxplot_movie_accuracy.pdf", bbox_inches="tight")
-fig2.savefig(figures_path / f"boxplot_task_accuracy_{N_PARCELS}.pdf", bbox_inches="tight", dpi=300)
+fig.savefig(figures_path / f"boxplot_task_accuracy_{N_PARCELS}.pdf", bbox_inches="tight", dpi=300)
 
 plt.show()
 
-# Do the same for the Pearson correlation
-df = get_results_dataframe(data_path, score="pearson_corrs")
-
-# Set the style and font scale for better readability
-plt.style.use(["science", "nature", "no-latex"])
-sns.set_context("paper", font_scale=1.3)
-
-# Create the figure and axes with a specific size
-fig, ax = plt.subplots(
-    figsize=(4.25, 6), constrained_layout=True
-)  # Increased height
-
-# Create the box plot
-sns.boxplot(
-    data=df,
-    x="pearson_corrs",
-    y="data_name",
-    hue="solver_name",
-    showfliers=False,
-    ax=ax,
-    fill=False,
-    legend=False,
-    palette="dark:k",
-)
-
-original_palette = sns.color_palette("Paired")
-shifted_palette = original_palette[1:] + original_palette[:1]
-
-# Create the scatter plot
-sns.stripplot(
-    data=df,
-    x="pearson_corrs",
-    y="data_name",
-    size=4,
-    hue="solver_name",
-    dodge=True,
-    jitter=True,
-    palette=shifted_palette,
-)
-
-# Customize the plot
-ax.set_xlabel("Pearson Correlation", fontweight="bold")
-ax.set_ylabel("Dataset", fontweight="bold")
-
-# Set and move the legend ABOVE the figure (not overlapping)
-legend = ax.legend(
-    title="Alignment method",
-    title_fontsize="large",
-    loc="lower center",
-    bbox_to_anchor=(0.3, -0.7),
-    frameon=False,
-)
-
-# Add gray rectangles to separate the datasets
-for i, data_name in enumerate(df["data_name"].unique()):
-    if i % 2 == 0:
-        ax.axhspan(i - 0.5, i + 0.5, color="gray", alpha=0.05)
-
-# Fix underscores in the dataset names
-ax.set_yticklabels(
-    [name.replace("_", " ") for name in df["data_name"].unique()]
-)
-
-# Save the figure with high resolution
-plt.savefig(
-    figures_path / f"boxplot_correlation_{N_PARCELS}.pdf", dpi=300, bbox_inches="tight"
-)
-
-# Display the plot
-plt.show()
--- benchmark_utils/solver_utils.py
+++ benchmark_utils/solver_utils.py
@@ -7,113 +7,37 @@ from sklearn.decomposition import PCA
 
 from benchmark_utils.datasets_utils import Dataset
 
-
-def compute_pca(dict_subjects: dict) -> np.ndarray:
-    data = np.vstack(list(dict_subjects.values()))
-    if data.shape[1] == 2:
-        return data
-    else:
-        pca = PCA(n_components=2)
-        return pca.fit_transform(data)
-
-
-def plot_pca(dataset: Dataset) -> None:
-    subjects = dataset.subjects
-    pca_unaligned = compute_pca(dataset.dict_decoding)
-    pca_aligned = compute_pca(dataset.dict_aligned)
-
-    # Create a single figure with 2 subplots
-    fig, ax = plt.subplots(2, 2, figsize=(10, 10))
-
-    legend_subjects = np.repeat(
-        subjects, dataset.dict_y[subjects[0]].shape[0]
-    )
-    legend_condition = np.concatenate(
-        [dataset.dict_y[subject] for subject in subjects]
-    )
-
-    # Define separate colormaps
-    subject_colormap = plt.cm.tab10  # Colormap for subjects
-    condition_colormap = plt.cm.viridis  # Colormap for conditions
-
-    # Map subjects to colors
-    subject_colors = {
-        subject: subject_colormap(i / len(subjects))
-        for i, subject in enumerate(subjects)
-    }
-    condition_colors = {
-        condition: condition_colormap(i / len(np.unique(legend_condition)))
-        for i, condition in enumerate(np.unique(legend_condition))
-    }
-
-    for subject in subjects:
-        ax[0, 0].scatter(
-            pca_unaligned[legend_subjects == subject, 0],
-            pca_unaligned[legend_subjects == subject, 1],
-            color=subject_colors[subject],
-            label=subject,
-        )
-        ax[0, 1].scatter(
-            pca_aligned[legend_subjects == subject, 0],
-            pca_aligned[legend_subjects == subject, 1],
-            color=subject_colors[subject],
-            label=subject,
-        )
-
-    for condition in np.unique(legend_condition):
-        ax[1, 0].scatter(
-            pca_unaligned[legend_condition == condition, 0],
-            pca_unaligned[legend_condition == condition, 1],
-            color=condition_colors[condition],
-            label=condition,
-        )
-        ax[1, 1].scatter(
-            pca_aligned[legend_condition == condition, 0],
-            pca_aligned[legend_condition == condition, 1],
-            color=condition_colors[condition],
-            label=condition,
-        )
-
-    # Add titles and legends
-    ax[0, 0].set_title("PCA of unaligned data")
-    ax[0, 1].set_title(f"PCA - {dataset.solver} - Target: {dataset.target_name}")
-    # Add legends to the right of the rightmost plots
-    ax[0, 1].legend(
-        loc="center left", bbox_to_anchor=(1, 0.5), title="Subjects"
-    )
-    ax[1, 1].legend(
-        loc="center left", bbox_to_anchor=(1, 0.5), title="Conditions"
-    )
-
-    # Adjust layout to fit legends
-    plt.tight_layout()
-    plt.subplots_adjust(right=0.85)  # Leave space for legends
-    # Save the figure
-    fig.savefig(
-        dataset.output_dir / "pca.png",
-        bbox_inches="tight",
-        dpi=300,
-    )
-
-
 def compute_alignment(
-    algo,
+    group_algo,
+    pairwise_algo,
     dataset: Dataset,
     solver_name: str,
 ) -> Dataset:
     dataset.solver = solver_name
-    output_dir = Path("outputs") / dataset.name / dataset.task_name / dataset.target_name / solver_name
+    output_dir = Path("outputs") / dataset.name / dataset.task_name / f"{dataset.test_sub}" / f"ext_template={str(dataset.external_template)}" / solver_name
     output_dir.mkdir(exist_ok=True, parents=True)
     dataset.output_dir = output_dir
     
-    # Time the alignment process
-    start_time = perf_counter()
-    algo.fit(dataset.dict_alignment, dataset.target)
-    dataset.dict_aligned = algo.transform(dataset.dict_decoding)
-    dataset.time = perf_counter() - start_time
+    if dataset.external_template is True:
+        test_sub_alignment = dataset.dict_alignment.pop(dataset.test_sub)
+        # Time the alignment process
+        start_time = perf_counter()
+        group_algo.fit(dataset.dict_alignment, "template")
+        dataset.time = perf_counter() - start_time        
+        template = group_algo.template
+        # Align the subjects to the template except the left-out subject
+        dataset.dict_aligned = group_algo.transform({k: v for k, v in dataset.dict_decoding.items() if k != dataset.test_sub})
+        
+        # Align the left-out subject to the template
+        pairwise_algo.fit(test_sub_alignment, template)
+        left_out_aligned = pairwise_algo.transform(dataset.dict_decoding[dataset.test_sub])
+        dataset.dict_aligned[dataset.test_sub] = left_out_aligned
+    else:
+        # Time the alignment process
+        start_time = perf_counter()
+        group_algo.fit(dataset.dict_alignment, "template")
+        dataset.time = perf_counter() - start_time
+        
+        dataset.dict_aligned = group_algo.transform(dataset.dict_decoding)
     
-    # Compute the PCA
-    print("Computing PCA")
-    plot_pca(dataset)
-    print("PCA computed")
     return dataset
