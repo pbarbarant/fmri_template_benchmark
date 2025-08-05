93c6e10550dcd12b6cf35d91dc7556702573c99e
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-08-05 09:42:14 +0200

    Adapt solver for out of sample template

--- solvers/ot.py
+++ solvers/ot.py
@@ -5,7 +5,7 @@ from benchopt import BaseSolver, safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     from benchopt.stopping_criterion import SingleRunCriterion
-    from fmralign import GroupAlignment
+    from fmralign import GroupAlignment, PairwiseAlignment
     from benchmark_utils.conf import N_JOBS
     from benchmark_utils.solver_utils import compute_alignment
     from fmralign.methods import OptimalTransport
@@ -45,15 +45,21 @@ class Solver(BaseSolver):
         # It runs the algorithm for a given a number of iterations `n_iter`.
         # You can also use a `tolerance` or a `callback`, as described in
         # https://benchopt.github.io/performance_curves.html
-        algo = GroupAlignment(
+        group_algo = GroupAlignment(
             method=OptimalTransport(reg=self.reg),
             labels=self.dataset.labels,
             n_jobs=N_JOBS,
             verbose=11,
         )
-
+        pairwise_algo = PairwiseAlignment(
+            method=OptimalTransport(reg=self.reg),
+            labels=self.dataset.labels,
+            n_jobs=N_JOBS,
+            verbose=11,
+        )
         self.dataset = compute_alignment(
-            algo,
+            group_algo,
+            pairwise_algo,
             self.dataset,
             solver_name=self.name,
         )
