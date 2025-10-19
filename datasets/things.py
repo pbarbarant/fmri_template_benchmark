5d8c448a24029226414251739b750978044f41dc
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-10-19 19:23:00 +0200

    Add things and forrest

---
+++ datasets/things.py
@@ -0,0 +1,50 @@
+from benchopt import BaseDataset, safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from benchmark_utils.datasets_utils import fetch_dataset, parse_subjects
+    from pathlib import Path
+
+
+# All datasets must be named `Dataset` and inherit from `BaseDataset`
+class Dataset(BaseDataset):
+    # Name to select the dataset in the CLI and to display the results.
+    name = "THINGS"
+
+    # List of packages needed to run the dataset. See the corresponding
+    # section in objective.py
+    install_pip = "pip"
+    requirements = []
+
+    data_path = (
+        Path(
+            "/data/parietal/store3/work/pbarbara/data/neuromod/z_maps/conditions"
+        )
+        / name
+    )
+
+    parameters = {
+        "n_parcels": [400],
+        "target": ["template_in_sample", "template_out_of_sample"]
+        + parse_subjects(data_path),
+    }
+
+    def get_data(self, data_path=data_path):
+        # The return arguments of this function are passed as keyword arguments
+        # to `Objective.set_data`. This defines the benchmark's
+        # API to pass data. It is customizable for each benchmark.
+
+        # The dictionary defines the keyword arguments for `Objective.set_data`
+
+        self.dataset = fetch_dataset(
+            name="Neuromod" + f"_{self.n_parcels}",
+            subjects=parse_subjects(data_path),
+            target=self.target,
+            data_path=data_path,
+            task=self.name,
+            n_parcels=self.n_parcels,
+        )
+
+        return dict(dataset=self.dataset)
