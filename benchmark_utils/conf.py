be6c6876ea54c2140698b476396de591c11c2219
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2025-10-24 00:59:13 +0200

    Remove unused paths in conf

--- benchmark_utils/conf.py
+++ benchmark_utils/conf.py
@@ -1,9 +1,3 @@
 N_JOBS = 30
 
-NEUROMOD_PATH = "/data/parietal/store4/data/cneuromod/"
-
-IBC_PATH = "/data/parietal/store2/data/ibc/3mm"
-
-IBC_SURF_PATH = "/data/parietal/store2/data/ibc/derivatives"
-
 IBC_GM_MASK = "/data/parietal/store3/work/pbarbara/public_analysis_code/ibc_data/gm_mask_3mm.nii.gz"
