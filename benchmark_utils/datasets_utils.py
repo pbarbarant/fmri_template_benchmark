86cbf58c6920cb12aec41c2194c9e1915d46303e
Pierre-Louis Barbarant <pierre-louis.barbarant@inria.fr>
2026-09-03 16:38:10 +0000

    Add list of decoding_subjects for hcp

--- benchmark_utils/datasets_utils.py
+++ benchmark_utils/datasets_utils.py
@@ -23,6 +23,7 @@ class Fold:
     dict_decoding: dict[str, np.ndarray]
     dict_y: dict[str, np.ndarray]
     dict_aligned: dict[str, np.ndarray] | None = None
+    decoding_subjects: list[str] | None = None
     time: float | None = None
 
 
