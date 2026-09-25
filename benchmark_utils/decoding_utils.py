3737c2524ffdff205564d6eccc574b943db66b4c
Pierre-Louis Barbarant <plbarbarant@mail.com>
2026-09-25 16:40:23 +0200

    Fix decoding_utils weights saving

--- benchmark_utils/decoding_utils.py
+++ benchmark_utils/decoding_utils.py
@@ -21,7 +21,7 @@ def save_weights(scores: dict, masker: NiftiMasker, output_dir: Path):
     )
 
     # Skip binary classification (simulation)
-    if classes_ > 2:
+    if len(classes_) > 2:
         for i, class_ in enumerate(classes_):
             masker.inverse_transform(coefs_aggregated[i]).to_filename(
                 output_dir / f"coefs_{class_}.nii.gz"
